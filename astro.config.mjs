6a3cbab6edd6822e61de7bdc538a39492b92fc96
Andre14052007 <andredsmatos@gmail.com>
2025-02-20 21:23:44 +0000

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
+});
